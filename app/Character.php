2048e94aa6ab3b7d2c9f6ba32cef29d8c7ca0ad7
Roy Admiraal <s1081752@student.hsleiden.nl>
2019-10-17 12:29:04 +0200

    Controller + models

--- app/Character.php
+++ app/Character.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Character extends Model
 {
     protected $table = 'character';
+
+    public function stats()
+    {
+        return $this->hasOne('App\Stat');
+    }
+
+
 }
+
--- app/Stat.php
+++ app/Stat.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Stat extends Model
 {
     protected $table = 'stats';
+
+    public function character()
+    {
+        return $this->belongsTo('App\Character');
+    }
+
+
+
 }
